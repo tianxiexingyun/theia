86e9a20099c6e928e77fba4b70d2386b34d5af06
tianxiexingyun <tianxiexingyun@qq.com>
2022-09-05 23:40:33 +0800

    prototype2
    Change-Id: I80a062f8a73ddaaa4459b797352cc537274a71ed

---
+++ prototype2/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+	<meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
+	<meta content="width=device-width" name="viewport">
+	<meta charset="utf-8">
+	<title>Theia</title>
+	<link rel="stylesheet" href="static/css/bootstrap.min.css">
+	<link rel="stylesheet" href="static/css/style.css">
+	<script src="static/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+	<div class="row">
+		<div class="col-xs-6 col-sm-3">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+		</div>
+		<div class="col-xs-6 col-sm-3">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+			   eiusmod tempor incididunt ut.
+			</p>
+		</div>
+
+		<div class="col-xs-6 col-sm-3">
+			<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			   laboris nisi ut aliquip ex ea commodo consequat.
+			</p>
+		</div>
+		<div class="col-xs-6 col-sm-3">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+			   eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+			   enim ad minim
+			</p>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
