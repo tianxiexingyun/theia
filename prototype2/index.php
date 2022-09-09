de2ba56aac7ad7e201c6ae9c5b115f5f67425532
tianxiexingyun <tianxiexingyun@qq.com>
2022-09-09 09:02:48 +0800

    prototype2
    Change-Id: I9a7d0e59ccdcdfbe1517e8a5d01da873bc1d7f93

--- prototype2/index.php
+++ prototype2/index.php
@@ -7,6 +7,7 @@
 	<title>Theia</title>
 	<link rel="stylesheet" href="static/css/bootstrap.min.css">
 	<link rel="stylesheet" href="static/css/style.css">
+	<script src="static/js/jquery-3.6.1.min.js"></script>
 	<script src="static/js/bootstrap.min.js"></script>
 </head>
 <body>
@@ -16,16 +17,35 @@
 		<div class="row banner">
 			<div class="col-xs-1 col-sm-1 col-md-1"></div>
 		</div>
-		<div class="row carousel-inner">
-			<div class="item active">
-				<img src="static/imgs/carousel.jpg" alt="First slide">
+		<div id="myCarousel" class="row carousel slide">
+			<!-- 轮播（Carousel）指标 -->
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+			</ol>
+			<!-- 轮播（Carousel）项目 -->
+			<div class="carousel-inner">
+				<div class="item active">
+					<img src="static/imgs/carousel.jpg" alt="First slide">
+				</div>
+				<div class="item">
+					<img src="static/imgs/carousel.jpg" alt="Second slide">
+				</div>
+				<div class="item">
+					<img src="static/imgs/carousel.jpg" alt="Third slide">
+				</div>
 			</div>
-			<div class="item">
-				<img src="static/imgs/carousel.jpg" alt="Second slide">
-			</div>
-			<div class="item">
-				<img src="static/imgs/carousel.jpg" alt="Third slide">
-			</div>
+
+			<!-- 轮播（Carousel）导航 -->
+			<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
 		</div>
 		<div class="row nav">
 			<div class="col-xs-1 col-sm-1 col-md-1"></div>
